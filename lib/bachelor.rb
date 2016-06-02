e6771d763b215c95075f4261d14c557b1597b025
David Wexler <dwexler@gmail.com>
2016-06-01 23:47:35 -0400

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,61 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  firstname = ""
+  data.each do |ssn, info|
+    if ssn == season
+      info.each do |ary|
+        if ary["status"] == "Winner"
+          nameary = ary["name"].split(" ")
+          firstname = nameary.shift
+        end
+      end
+    end
+  end
+  firstname
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, info|
+    info.each do |contestant|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, info|
+    info.each do |contestant|
+      if contestant["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  people = []
+  data.each do |season, info|
+    info.each do |contestant|
+      if contestant["hometown"] == hometown
+        people << contestant["occupation"]
+      end
+    end
+  end
+  people.shift
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+  data.each do |ssn, info|
+    if ssn == season
+      info.each do |contestant|
+        ages << contestant["age"].to_i
+      end
+    end
+  end
+  avg = ages.inject(:+) / ages.length.to_f
+  avg.round
 end
